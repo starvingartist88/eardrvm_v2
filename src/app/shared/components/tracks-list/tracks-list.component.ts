6d8d00b1d86bcfaa7d0c827b5057e8e7cb4d42e7
Maham Moiz <mahammoiz@Mahams-MacBook-Air.local>
2019-08-13 00:35:34 +0500

    removed console and formated the tracks component file

--- src/app/shared/components/tracks-list/tracks-list.component.ts
+++ src/app/shared/components/tracks-list/tracks-list.component.ts
@@ -1,54 +1,61 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-import { Track } from '../../../tracks/models/track.model';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import { MatTable } from '@angular/material/table';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild
+} from "@angular/core";
+import { Track } from "../../../tracks/models/track.model";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { MatTable } from "@angular/material/table";
 
 @Component({
-  selector: 'app-tracks-list',
-  templateUrl: './tracks-list.component.html',
-  styleUrls: ['./tracks-list.component.scss']
+  selector: "app-tracks-list",
+  templateUrl: "./tracks-list.component.html",
+  styleUrls: ["./tracks-list.component.scss"]
 })
 export class TracksListComponent implements OnInit {
   @Input() tracks: Track[];
   @Output() trackDeleted = new EventEmitter<Track>();
   @Output() trackEdited = new EventEmitter<Track>();
   @Output() trackEditSortNumber = new EventEmitter<Track>();
-  
-  @ViewChild('table') table: MatTable<Track>;
-  
-  displayedColumns: string[] = ['url', 'name', 'description', 'album', 'action']
+
+  @ViewChild("table") table: MatTable<Track>;
+
+  displayedColumns: string[] = [
+    "url",
+    "name",
+    "description",
+    "album",
+    "action"
+  ];
   // sortedTracks: any;
   constructor() {}
-  
-  ngOnInit() {
-    // this.tracks=this.tracks.sort((a, b) => a.sortNumber < b.sortNumber ? -1 : a.sortNumber > b.sortNumber ? 1 : 0);
-  }
-  
+
+  ngOnInit() {}
+
   onEdit(track: Track) {
     this.trackEdited.emit(track);
   }
-  
+
   onDelete(track: Track) {
     this.trackDeleted.emit(track);
   }
-  
+
   trackByFn(index: any) {
     return index;
   }
-  
+
   dropTable(event: CdkDragDrop<Track[]>) {
-    const prevIndex = this.tracks.findIndex((d) => d === event.item.data);
+    const prevIndex = this.tracks.findIndex(d => d === event.item.data);
     moveItemInArray(this.tracks, prevIndex, event.currentIndex);
     this.table.renderRows();
-    
-    let i=1;
+
+    let i = 1;
     this.tracks.forEach(element => {
-      element['sortNumber']=i++;
+      element["sortNumber"] = i++;
       this.trackEditSortNumber.emit(element);
     });
-    
-    console.log(this.tracks)
-    
   }
 }
-
